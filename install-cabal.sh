57742eb177208e7fe055cefb5a8c643dcfbf23a8
Dambaev Alexander <ice.redmine@gmail.com>
2018-08-10 16:15:29 +0800

    add installation script, which is using cabal in case of absence of stack (like on armhf)

---
+++ install-cabal.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+cabal sandbox init
+cabal update
+# hspec will be needed in order to run tests
+cabal install hspec
+# this will built xi-ctm in the .cabal-sandbox dir
+cabal install
+# this will test xi-ctm
+cabal test
+# now actually install xi-ctm
+cp .cabal-sandbox/bin/xi-ctm /usr/local/bin/
+
